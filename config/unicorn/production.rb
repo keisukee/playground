2c1f30a5117e11b7b9beaed71478cc03621200eb
keisuke ohashi <kog71903@gmail.com>
2019-08-04 17:57:41 +0900

    Change unicorn/production.rb

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,41 +1,32 @@
-#ワーカーの数。後述
+# set lets
 $worker  = 2
-#何秒経過すればワーカーを削除するのかを決める
-  $timeout = 30
-#自分のアプリケーション名、currentがつくことに注意。
-  $app_dir = "/var/www/mumu/current"
-#リクエストを受け取るポート番号を指定。後述
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-#PIDの管理ファイルディレクトリ
-  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-#エラーログを吐き出すファイルのディレクトリ
-  $std_log = File.expand_path 'log/unicorn.log', $app_dir
-
-# 上記で設定したものが適応されるよう定義
-  worker_processes  $worker
-  working_directory $app_dir
-  stderr_path $std_log
-  stdout_path $std_log
-  timeout $timeout
-  listen  $listen
-  pid $pid
-
-#ホットデプロイをするかしないかを設定
-  preload_app true
-
-#fork前に行うことを定義。後述
-  before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-    old_pid = "#{server.config[:pid]}.oldbin"
-    if old_pid != server.pid
-      begin
-        Process.kill "QUIT", File.read(old_pid).to_i
-      rescue Errno::ENOENT, Errno::ESRCH
-      end
+$timeout = 30
+$app_dir = "/var/www/rails/playground/current" #自分のアプリケーション名
+$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
+# set config
+worker_processes  $worker
+working_directory $app_dir
+stderr_path $std_log
+stdout_path $std_log
+timeout $timeout
+listen  $listen
+pid $pid
+# loading booster
+preload_app true
+# before starting processes
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
-
-#fork後に行うことを定義。後述
-  after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-  end
+end
+# after finishing processes
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
